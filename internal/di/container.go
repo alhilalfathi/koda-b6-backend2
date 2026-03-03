75e5d23f4948a03da460ca15a88417a0597ee9af
alhilalfathi <alhilal.fathi@gmail.com>
2026-03-03 15:17:25 +0700

    feat: change typo handler

--- internal/di/container.go
+++ internal/di/container.go
@@ -29,7 +29,7 @@ func NewContainer() *Container {
 func (c *Container) initDependencies() {
 	c.userRepo = repository.NewUserRepository(c.user)
 	c.userService = service.NewUserService(c.userRepo)
-	c.userHandler = handlers.NewUserHanler(c.userService)
+	c.userHandler = handlers.NewUserHandler(c.userService)
 }
 
 func (c *Container) UserHandler() *handlers.UserHandler {
